3aac506257c478bcb8391c83be81e0c1aa773c6c
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-08-11 22:49:27 -0400

    calculo de montos

---
+++ data.js
@@ -0,0 +1,22 @@
+export const pagos = [
+  {
+    id: 1,
+    nombre_concepto: "Matricula Posgrado",
+    costo: "1000.00",
+    porcentaje_descuento: "0.00",
+    descuento: "0.00",
+    monto_pagar: "1000.00",
+    desglose: true,
+    aplica_descuento: false,
+  },
+  {
+    id: 2,
+    nombre_concepto: "Colegiatura Posgrado",
+    costo: "300.00",
+    porcentaje_descuento: "10.00",
+    descuento: "30.00",
+    monto_pagar: "270.00",
+    desglose: true,
+    aplica_descuento: true,
+  },
+];
--- main.js
+++ main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { pagos } from "./data";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+pagos.forEach((item, indice) => {
+  let fila = /*html */ `
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.nombre_concepto}</td>
+            <td>
+              <input type="number" value="${item.costo}" class="costo">
+            </td>
+            <td>
+              <select class="porcentaje_descuento" ${
+                item.aplica_descuento ? "" : "disabled"
+              }>
+                <option value="0" ${
+                  item.porcentaje_descuento == 0 ? "selected" : ""
+                }>0%</option>
+                <option value="10" ${
+                  item.porcentaje_descuento == 10 ? "selected" : ""
+                }>10%</option>
+                <option value="20" ${
+                  item.porcentaje_descuento == 20 ? "selected" : ""
+                }>20%</option>
+              </select>
+            </td>
+            <td>
+              <input type="number" value="${
+                item.descuento
+              }" class="descuento" readonly>
+            </td>
+            <td>
+              <input type="number" value="${
+                item.monto_pagar
+              }" class="monto_pagar" readonly>
+            </td>
+            <td>
+              <input type="checkbox" value="${item.desglose}" class="desglose">
+            </td>
+        </tr>
+    `;
 
-setupCounter(document.querySelector('#counter'))
+  document.querySelector("#tbody").innerHTML += fila;
+});
+
+function cambiarMontos(indice) {
+  const costo = document.querySelectorAll(`.costo`)[indice].value;
+  const porcentaje_descuento = document.querySelectorAll(
+    `.porcentaje_descuento`
+  )[indice].value;
+  const montoDescuento = (costo * (porcentaje_descuento / 100)).toFixed(2);
+
+  document.querySelectorAll(`.descuento`)[indice].value = montoDescuento;
+  document.querySelectorAll(`.monto_pagar`)[indice].value = (
+    costo - montoDescuento
+  ).toFixed(2);
+}
+
+document.querySelectorAll(".costo").forEach((item, indice) => {
+  item.addEventListener("input", () => {
+    cambiarMontos(indice);
+  });
+});
+
+document.querySelectorAll(".porcentaje_descuento").forEach((item, indice) => {
+  item.addEventListener("change", () => {
+    cambiarMontos(indice);
+  });
+});
+
+//<input type="number" value="${item.porcentaje_descuento}" class="porcentaje_descuento" ${item.aplica_descuento ? '' : 'readonly' } min="0" max="100" step="1">
